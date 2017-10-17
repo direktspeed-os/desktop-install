4eedbc3a29435fd7e8220e25544df28771e26122
Frank Lemanschik <frank@github.dspeed.eu>
2017-10-17 12:29:08 +0100

    Create arm.sh

---
+++ arm.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set +x
+set -e
+# TODO: Detect display driver.
+echo ""
+echo "Installing basic X11 packages on DIREKTSPEED-OS"
+
+# --------------------------------------------------------------------
+echo ""
+echo " STEP 1: create a /boot/config.txt"
+CONFIG_TXT_FILE=/boot/config.txt
+if [ ! -f ${CONFIG_TXT_FILE} ]; then
+
+cat << EOF | sudo tee ${CONFIG_TXT_FILE} >/dev/null
+display_rotate=0	# normal HDMI displays
+#display_rotate=2	# 7" Touch Screen display from RaspberryPi.Org
+EOF
+
+fi
+cat ${CONFIG_TXT_FILE}
+echo "...done"
+
+# --------------------------------------------------------------------
+echo ""
+echo " STEP 2: enable LightDM autologin for user=pirate"
+LIGHTDM_CONF_FILE=/etc/lightdm/lightdm.conf
+if [ ! -f ${LIGHTDM_CONF_FILE}.sav ]; then
+  # backup original file
+  sudo mv ${LIGHTDM_CONF_FILE} ${LIGHTDM_CONF_FILE}.sav
+
+cat << EOF | sudo tee ${LIGHTDM_CONF_FILE} >/dev/null
+[SeatDefaults]
+autologin-user=pirate
+autologin-user-timeout=0
+EOF
+
+fi
+cat ${LIGHTDM_CONF_FILE}
+echo "...done"
+
+
+# --------------------------------------------------------------------
+#echo ""
+#echo " STEP 4: install pre-compiled fbturbo"
+#FBTURBO_TAR_URL=https://github.com/hypriot/x11-on-HypriotOS/raw/master/fbturbo/fbturbo.tar.gz
+#FBTURBO_TAR_TMPFILE=/tmp/fbturbo.tar.gz
+#wget -O ${FBTURBO_TAR_TMPFILE} ${FBTURBO_TAR_URL}
+#sudo tar xvf ${FBTURBO_TAR_TMPFILE} -C /
+#rm -f ${FBTURBO_TAR_TMPFILE}
+#echo "...done"
+
+
+# --------------------------------------------------------------------
+echo ""
+echo "...X11 installation done, please reboot"
